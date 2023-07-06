ae7c7a5eab21cd23de0d51f6212914a81df83a7a
小志 <895180729@qq.com>
2023-07-06 13:02:29 +0800

    适配 postgres

--- distribution/conf/postgres-schema.sql
+++ distribution/conf/postgres-schema.sql
@@ -456,7 +456,7 @@ create table public.permissions
 alter table public.permissions
     owner to postgres;
 
-
+-- 用户名：nacos 密码：nacos
 INSERT INTO users (username, password, enabled) VALUES ('nacos', '$2a$10$EuWPZHzz32dJN7jexM34MOeYirDdFAZm2kuWj7VEOJhhZkDrxfvUu', TRUE);
 
 INSERT INTO roles (username, role) VALUES ('nacos', 'ROLE_ADMIN');
